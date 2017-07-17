42636c009aa3875312b1e558574d97745c68c157
Dusty McCord <dustymccord@gmail.com>
2017-07-17 13:47:42 -0700

    added template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Portfolio from './Components/Portfolio';
+import Resume from './Components/Resume';
+import Testimonials from './Components/Testimonials';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My App
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <Contact />
+        <Footer />
+
       </div>
     );
   }
